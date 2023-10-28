feda226e07f477297e2abd497e4f8ce4f3b29542
Pedro <pedro.prccau@gmail.com>
2023-10-28 20:19:16 -0300

    pica

--- projetoCladro/index.php
+++ projetoCladro/index.php
@@ -4,21 +4,19 @@
     <head>
         <meta charset="UTF-8">
         <title>Log in</title>
+        <link href="./css/estilo.css" rel="stylesheet">
     </head>
     <body>
         <header>
             
         </header>
         
-        
-         
-        
         <div class="cadastro">
             <h2>E-mail:</h2>
             <input type="email" name="email" id="email">
             <h2>Senha:</h2>
             <input type="password" name="senha" id="senha">
-            
+            <input class="entrar" type="submit" value="Entrar">
         </div>
         
         
